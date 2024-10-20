93830802fe1601ef700bfe91c82058b1ccd6cb1f
Bigyan <bigyan8aryal@gmail.com>
2024-10-20 22:51:33 +0545

    search functionality added

--- chatbud/chatapp/views.py
+++ chatbud/chatapp/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 def index(request):
-    rooms = Room.objects.all()
-    context = {'rooms':rooms}
+    q=request.GET.get('q') if request.GET.get('q') != None else ''
+    # rooms = Room.objects.filter(topic__name__icontains = q) #topic__name__icontains this basically helps in searching eg if we search for python and type py it looks that it matches and shows the result for python.
+    # a better feature would be for a user to be able to search by the topic or the title or the description of the room.in order to do so, we import query q and then put some or conditions.
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    ) 
+    room_count = rooms.count()
+    topics = Topic.objects.all()
+    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count}
     return render(request,'chatapp/home.html',context)
 
 def room(request,pk):
