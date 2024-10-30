9b2beaee63e8ddfea6e5e19c57504408c9837682
Bigyan <bigyan8aryal@gmail.com>
2024-10-30 22:37:28 +0545

    custom user model

--- chatbud/chatapp/models.py
+++ chatbud/chatapp/models.py
@@ -1,8 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-
+class User(AbstractUser):
+    name = models.CharField(max_length=200,null=True)
+    email = models.EmailField(unique=True,null=True)
+    bio = models.TextField(null=True)
+    avatar= models.ImageField(null=True, default="avatar.svg")
+    USERNAME_FIELD= 'email'
+    REQUIRED_FIELDS=[]
+# In this section we used default User model from the django i.e. '
+# from django.contrib.auth.models import User' but now we comment all out and then create a custom user model above all these comments. basically the django user model is replaced by custom user model.
 # a topic can have multiple rooms, but a room can have a single topic
 class Topic(models.Model):
     name = models.CharField(max_length=200)
@@ -39,3 +47,5 @@ class Message(models.Model):
 
     def __str__(self):
         return self.body[0:50] #this will provide only the preview of the body and not all the contents.
+
+
--- chatbud/chatapp/views.py
+++ chatbud/chatapp/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Room, Topic, Message
+from .models import Room, Topic, Message, User
 from .forms import RoomForm, UserForm
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required #this decorator is used to restrict some of the pages from accessing without loggin in, like creating and updating rooms and redirecting them to the login or singup page.
