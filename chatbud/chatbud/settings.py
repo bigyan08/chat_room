4abf009250a3212549676866a034f4cf5132d4c6
Bigyan <bigyan8aryal@gmail.com>
2024-11-01 22:44:47 +0545

    env setup

--- chatbud/chatbud/settings.py
+++ chatbud/chatbud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f2ees%92t+psf1d8=968l@#k0_uw%$+q7+o&6=g^!a974xfks%'
+SECRET_KEY =os.environ['SECRET_KEY_ENV']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -134,3 +135,10 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_USE_TLS = True  
+EMAIL_HOST = 'smtp.gmail.com'  
+EMAIL_HOST_USER = 'bigyan8aryal@gmail.com'  
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD']  
+EMAIL_PORT = 587  
