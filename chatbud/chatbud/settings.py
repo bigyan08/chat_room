e6859df3fd512958af5be6b6abd4d1882b42c13c
Bigyan <bigyan8aryal@gmail.com>
2024-11-03 22:32:11 +0545

    database setup

--- chatbud/chatbud/settings.py
+++ chatbud/chatbud/settings.py
@@ -79,13 +79,17 @@ WSGI_APPLICATION = 'chatbud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DB_NAME'],  # Replace with your actual database name from Supabase
+        'USER': os.environ['DB_USER'],  # Replace with your Supabase username
+        'PASSWORD': os.environ['DB_PASSWORD'],  # Replace with your Supabase password
+        'HOST': os.environ['DB_HOST'],  # Replace with your Supabase host
+        'PORT': os.environ['PORT'],  # Default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
